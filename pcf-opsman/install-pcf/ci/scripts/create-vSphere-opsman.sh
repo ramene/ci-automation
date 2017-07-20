a1f40141ecb5b6fd53dff282c1b1c004dd546477
Ramene Anthony <github@jadedgeek.com>
2017-07-20 16:07:08 -0400

    initial commit

--- pcf-opsman/install-pcf/ci/scripts/create-vSphere-opsman.sh
+++ pcf-opsman/install-pcf/ci/scripts/create-vSphere-opsman.sh
@@ -21,14 +21,13 @@ fi
 ovf_cmd="ovftool --name='Ops Manager - $OPSMAN_VERSION' \
         -nw=$NETWORK_NAME \
         -ds=$DATA_STORE_NAME \
-        -dm=thin \
+        -dm=thick \
         --prop:ip0=$OPSMAN_IP \
         --prop:netmask0=$OPSMAN_NETMASK \
         --prop:gateway=$OPSMAN_GATEWAY \
         --prop:DNS=$OPSMAN_DNS \
         --prop:ntp_servers=$OPSMAN_NTP \
         --prop:admin_password=$OPSMAN_VM_PASS \
-        --powerOn \
         --noSSLVerify \
         --acceptAllEulas \
         --sourceType=ova \
