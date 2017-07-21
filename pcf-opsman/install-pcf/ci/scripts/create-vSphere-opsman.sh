a94ce86328f05f976be442ea696054d768e0f1bd
Ramene Anthony <ramene@users.noreply.github.com>
2017-07-21 12:13:02 -0400

    Update create-vSphere-opsman.sh

--- pcf-opsman/install-pcf/ci/scripts/create-vSphere-opsman.sh
+++ pcf-opsman/install-pcf/ci/scripts/create-vSphere-opsman.sh
@@ -11,7 +11,7 @@ echo "==========================================================================
 echo "Executing ovf command ...."
 vcenter_user=${VCENTER_USER//\\/%5c}
 vcenter_user=${vcenter_user/@/%40}
-vcenter_pass=${VCENTER_PASSWORD//\\/%5c}
+vcenter_pass=\"${VCENTER_PASSWORD//\\/%5c}\"
 vcenter_pass=${vcenter_pass/@/%40}
 
 if [[ $IAAS == "vSphere" ]]; then
@@ -25,8 +25,8 @@ ovf_cmd="ovftool --name='Ops Manager - $OPSMAN_VERSION' \
         --prop:ip0=$OPSMAN_IP \
         --prop:netmask0=$OPSMAN_NETMASK \
         --prop:gateway=$OPSMAN_GATEWAY \
-        --prop:DNS="$OPSMAN_DNS" \
-        --prop:ntp_servers="$OPSMAN_NTP" \
+        --prop:DNS=\"$OPSMAN_DNS\" \
+        --prop:ntp_servers=\"$OPSMAN_NTP\" \
         --prop:admin_password=$OPSMAN_VM_PASS \
         --noSSLVerify \
         --acceptAllEulas \
