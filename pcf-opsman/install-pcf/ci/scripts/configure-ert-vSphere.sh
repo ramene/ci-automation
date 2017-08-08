fd75d6621163131ba8f49120d3c76f333df9d889
Ramene Anthony <github@jadedgeek.com>
2017-08-08 08:42:26 -0400

    updated ERT product config

--- pcf-opsman/install-pcf/ci/scripts/configure-ert-vSphere.sh
+++ pcf-opsman/install-pcf/ci/scripts/configure-ert-vSphere.sh
@@ -72,9 +72,12 @@ CF_AUTH_PROPERTIES=$(cat <<-EOF
   },
   ".uaa.service_provider_key_credentials": {
     "value": {
-      "cert_pem": "$SSL_CERT",
-      "private_key_pem": "$SSL_PRIVATE_KEY"
+      "cert_pem": "$SVCPROVIDER_SSL_CERT",
+      "private_key_pem": "$SVCPROVIDER_SSL_KEY"
     }
+  },
+  ".properties.uaa.ldap.server_ssl_cert": {
+    "value": "$ERT_SSL_CERT"
   },
   ".properties.uaa.ldap.url": {
     "value": "$LDAP_URL"
