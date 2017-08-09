c84f4f22fa9c2e93d95e2d7c03d9cdb3808230b1
Ramene Anthony <github@jadedgeek.com>
2017-08-09 05:14:03 -0400

    updated product configs for APM, JMX and AppDynamics

--- pcf-opsman/install-pcf/ci/scripts/configure-mysql.sh
+++ pcf-opsman/install-pcf/ci/scripts/configure-mysql.sh
@@ -21,9 +21,6 @@ sed -i -e "s|{{MYSQL_NETWORK}}|${MYSQL_NETWORK}|g" ${json_file}
 sed -i -e "s|{{MYSQL_SINGLETON_AZ}}|${MYSQL_SINGLETON_AZ}|g" ${json_file}
 sed -i -e "s|{{MYSQL_EMAIL}}|${MYSQL_EMAIL}|g" ${json_file}
 sed -i -e "s|{{MYSQL_SYSLOG_ADDRESS}}|${MYSQL_SYSLOG_ADDRESS}|g" ${json_file}
-# APM
-sed -i -e "s|{{APM_ALERTS_EMAIL}}|${APM_ALERTS_EMAIL}|g" ${json_file}
-
 
 function fn_om_linux_curl {
 
